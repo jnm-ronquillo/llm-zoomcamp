3633323ddcadeebfee741ae0227fff719ca4f055
Juan Manuel Rojas Ronquillo <55019923+jnm-ronquillo@users.noreply.github.com>
2024-07-15 15:33:03 +0000

    hw 3

---
+++ cohorts/2024/03-vector-search/util_evaluate.py
@@ -0,0 +1,23 @@
+from tqdm.auto import tqdm
+
+def hit_rate(relevance_total):
+    cnt = 0
+
+    for line in relevance_total:
+        if True in line:
+            cnt = cnt + 1
+
+    return cnt / len(relevance_total)
+
+def evaluate(ground_truth, search_function):
+    relevance_total = []
+
+    for q in tqdm(ground_truth):
+        doc_id = q['document']
+        results = search_function(q)
+        relevance = [d['id'] == doc_id for d in results]
+        relevance_total.append(relevance)
+
+    return {
+        'hit_rate': hit_rate(relevance_total),
+    }
